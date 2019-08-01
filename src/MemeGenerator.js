cd36dad73ed9310213b2de6ca05b960ef5ce1155
stelian nechita <steliannechita2013@gmail.com>
2019-08-01 23:08:45 +0100

    font size change and responsive

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -1,66 +1,83 @@
-import React,{Component} from 'react'
+import React, { Component } from "react";
 
-class MemeGenerator extends Component{
-    constructor(){
-        super()
-        this.state={
-            topText:'One does not simply walk into Mordor',
-            bottomText:'to kill sauron with a wooden sword',
-            randomImg:'http://i.imgflip.com/1bij.jpg',
-            allMemeImgs:[]
-          }
-    }
-    handleChange=(event)=>{
-    const {name,value}=event.target
-    this.setState({[name]:value})      
-    }
-    handleClick=()=>{
-        let randomNumber= Math.floor(Math.random()*this.state.allMemeImgs.length)
-        this.setState({randomImg:this.state.allMemeImgs[randomNumber].url})
-    }
-    componentDidMount(){
-        fetch("https://api.imgflip.com/get_memes")
-        .then(data=>data.json())
-        .then(response=>{
-            const{memes}=response.data
-            this.setState({allMemeImgs:memes})
-        })
-        
-    }
-    render(){
-       return(
-        
-        <div>
-            <from className='meme-form'>
-                <input 
-                type ='text' 
-                name='topText'
-                placeholder='top text'
-                onChange={this.handleChange}
-                value={this.state.topText}
-                />
-                <br/>
-                <input 
-                type ='text' 
-                name='bottomText'
-                placeholder='bottom text'
-                onChange={this.handleChange}
-                value={this.state.bottomText}
-                />
-                <button onClick={this.handleClick}>Generate!</button>
-            </from>
-            <div className='meme'>
-                <h2 className='top'>{this.state.topText}</h2>
-                <img src={this.state.randomImg} alt=''/>
-                
-                <h2 className='bottom'>{this.state.bottomText}</h2>
-            </div>   
-        
-        
-          
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {
+      font_size: "",
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: []
+    };
+  }
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+  handleClick = () => {
+    let randomNumber = Math.floor(
+      Math.random() * this.state.allMemeImgs.length
+    );
+    this.setState({ randomImg: this.state.allMemeImgs[randomNumber].url });
+  };
+  increaseFont = () => {};
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(data => data.json())
+      .then(response => {
+        const { memes } = response.data;
+        this.setState({ allMemeImgs: memes });
+      });
+  }
+  render() {
+    console.log(this.state.font_size);
+    return (
+      <div>
+        <div className="meme-form">
+          <input
+            type="text"
+            name="topText"
+            placeholder="top text"
+            onChange={this.handleChange}
+            value={this.state.topText}
+          />
+          <br />
+          <input
+            type="text"
+            name="bottomText"
+            placeholder="bottom text"
+            onChange={this.handleChange}
+            value={this.state.bottomText}
+          />
+          <br />
+          <input
+            type="number"
+            name="font_size"
+            placeholder="font size"
+            onChange={this.handleChange}
+            value={this.state.font_size}
+          />
+          <button onClick={this.handleClick}>Generate!</button>
         </div>
-       ) 
-    }
+        <div className="meme">
+          <h2
+            style={{ fontSize: Number(this.state.font_size) }}
+            className="top"
+          >
+            {this.state.topText}
+          </h2>
+          <img src={this.state.randomImg} alt="" />
+          <h2
+            style={{ fontSize: Number(this.state.font_size) }}
+            className="bottom"
+          >
+            {this.state.bottomText}
+          </h2>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MemeGenerator
+export default MemeGenerator;
