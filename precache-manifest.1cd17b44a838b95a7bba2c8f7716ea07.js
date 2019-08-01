3692acc051de89c66687ec4792ff9fbe4ff53a77
stelian nechita <steliannechita2013@gmail.com>
2019-08-01 14:47:25 +0100

    Updates

---
+++ precache-manifest.1cd17b44a838b95a7bba2c8f7716ea07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab94c9807521c5bf191de6693a96acf3",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "2e61e1a71484ee2eb87f",
+    "url": "/meme-generator/static/css/main.fc637f0f.chunk.css"
+  },
+  {
+    "revision": "e90574d74274c7fb4d49",
+    "url": "/meme-generator/static/js/2.c39fed63.chunk.js"
+  },
+  {
+    "revision": "2e61e1a71484ee2eb87f",
+    "url": "/meme-generator/static/js/main.68b877a1.chunk.js"
+  },
+  {
+    "revision": "b8372d4f76d85c66a91a",
+    "url": "/meme-generator/static/js/runtime~main.0d1394e5.js"
+  }
+]);
